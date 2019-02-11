c248e5dc5094eab726880442735ef7051d34d46d
Nathan Andrus <nathanpandrus@gmail.com>
2019-02-11 12:14:20 -0700

    Initial Commit

---
+++ JournalObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JournalObjectiveC
+//
+//  Created by Nathan Andrus on 2/11/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JournalObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JournalObjectiveC
+//
+//  Created by Nathan Andrus on 2/11/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
